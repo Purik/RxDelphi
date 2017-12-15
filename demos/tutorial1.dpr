b8d93e45729ca1d4f7d25f36ff3c674e33e67240
Pavel_Minenkov <minikspb@gmail.com>
2017-12-15 16:27:06 +0300

    first tutorial

--- demos/tutorial1.dpr
+++ demos/tutorial1.dpr
@@ -8,11 +8,36 @@ uses
   Rx,
   System.SysUtils;
 
+var
+  OnData: TOnNext<TZip<LongWord, string>>;
+  OnCompleted: TOnCompleted;
+
+  Timer: TObservable<LongWord>;
+  Input: TObservable<string>;
+  Output: TObservable<TZip<LongWord, string>>;
+
 begin
-  try
-    { TODO -oUser -cConsole Main : Insert code here }
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
+
+  OnData := procedure(const Data: TZip<LongWord, string>)
+  begin
+    WriteLn('');
+    WriteLn('Timestamp: ' + TimeToStr(Now));
+    WriteLn(Format('iter: %d     value: %s', [Data.A, Data.B]))
   end;
+
+  OnCompleted := procedure
+  begin
+    WriteLn('');
+    WriteLn('Press ENTER to Exit');
+    Readln;
+  end;
+
+  Timer := Observable.Interval(1, TimeUnit.SECONDS);
+  Input := TObservable<string>.Just(['one', 'two', 'three', 'four', 'five']);
+
+  { Build result data stream }
+  Output := Observable.Zip<LongWord, string>(Timer, Input);
+  Output.Subscribe(OnData, OnCompleted);
+  Output.WaitCompletition(INFINITE);
+
 end.
